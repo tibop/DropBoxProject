153c12751e48bb863d010d9b46db8c842d2bd1a9
Thibaut <tibo@NoxA15-Tibo>
2012-12-01 16:55:53 +0100

    1ere version avec authentification

---
+++ Sources/client.c
@@ -0,0 +1,304 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <ctype.h>
+#include <netdb.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/select.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <errno.h>
+
+#include "client.h"
+
+#define TRUE 1
+#define FALSE 0
+
+#define LONGUEUR_TAMPON 4096
+
+/* Variables cachees */
+
+/* le socket client */
+int socketClient;
+/* le tampon de reception */
+char tamponClient[LONGUEUR_TAMPON];
+int debutTampon;
+int finTampon;
+
+/* Initialisation.
+ * Connexion au serveur sur la machine donnee.
+ * Utilisez localhost pour un fonctionnement local.
+ */
+int Initialisation(char *machine) {
+	return InitialisationAvecService(machine, "13214");
+}
+
+/* Initialisation.
+ * Connexion au serveur sur la machine donnee et au service donne.
+ * Utilisez localhost pour un fonctionnement local.
+ */
+int InitialisationAvecService(char *machine, char *service) {
+	int n;
+	struct addrinfo	hints, *res, *ressave;
+
+	bzero(&hints, sizeof(struct addrinfo));
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+
+	if ( (n = getaddrinfo(machine, service, &hints, &res)) != 0)  {
+     		fprintf(stderr, "Initialisation, erreur de getaddrinfo : %s", gai_strerror(n));
+     		return 0;
+	}
+	ressave = res;
+
+	do {
+		socketClient = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+		if (socketClient < 0)
+			continue;	/* ignore this one */
+
+		if (connect(socketClient, res->ai_addr, res->ai_addrlen) == 0)
+			break;		/* success */
+
+		close(socketClient);	/* ignore this one */
+	} while ( (res = res->ai_next) != NULL);
+
+	if (res == NULL) {
+     		perror("Initialisation, erreur de connect.");
+     		return 0;
+	}
+
+	freeaddrinfo(ressave);
+	printf("Connexion avec le serveur reussie.\n");
+
+	return 1;
+}
+
+/* Recoit un message envoye par le serveur.
+ */
+char *Reception() {
+	char message[LONGUEUR_TAMPON];
+	int index = 0;
+	int fini = FALSE;
+	int retour = 0;
+	while(!fini) {
+		/* on cherche dans le tampon courant */
+		while((finTampon > debutTampon) && 
+			(tamponClient[debutTampon]!='\n')) {
+			message[index++] = tamponClient[debutTampon++];
+		}
+		/* on a trouve ? */
+		if ((index > 0) && (tamponClient[debutTampon]=='\n')) {
+			message[index++] = '\n';
+			message[index] = '\0';
+			debutTampon++;
+			fini = TRUE;
+			return strdup(message);
+		} else {
+			/* il faut en lire plus */
+			debutTampon = 0;
+			retour = recv(socketClient, tamponClient, LONGUEUR_TAMPON, 0);
+			if (retour < 0) {
+				perror("Reception, erreur de recv.");
+				return NULL;
+			} else if(retour == 0) {
+				fprintf(stderr, "Reception, le serveur a ferme la connexion.\n");
+				return NULL;
+			} else {
+				/*
+				 * on a recu "retour" octets
+				 */
+				finTampon = retour;
+			}
+		}
+	}
+	return NULL;
+}
+
+/* Envoie un message au serveur.
+ * Attention, le message doit etre termine par \n
+ */
+int Emission(char *message) {
+	if(strstr(message, "\n") == NULL) {
+		fprintf(stderr, "Emission, Le message n'est pas termine par \\n.\n");
+	}
+	int taille = strlen(message);
+	if (send(socketClient, message, taille,0) == -1) {
+        perror("Emission, probleme lors du send.");
+        return 0;
+	}
+	printf("Emission de %d caracteres.\n", taille+1);
+	return 1;
+}
+
+/* Recoit des donnees envoyees par le serveur.
+ */
+int ReceptionBinaire(char *donnees, size_t tailleMax) {
+	int dejaRecu = 0;
+	int retour = 0;
+	/* on commence par recopier tout ce qui reste dans le tampon
+	 */
+	while((finTampon > debutTampon) && (dejaRecu < tailleMax)) {
+		donnees[dejaRecu] = tamponClient[debutTampon];
+		dejaRecu++;
+		debutTampon++;
+	}
+	/* si on n'est pas arrive au max
+	 * on essaie de recevoir plus de donnees
+	 */
+	if(dejaRecu < tailleMax) {
+		retour = recv(socketClient, donnees + dejaRecu, tailleMax - dejaRecu, 0);
+		if(retour < 0) {
+			perror("ReceptionBinaire, erreur de recv.");
+			return -1;
+		} else if(retour == 0) {
+			fprintf(stderr, "ReceptionBinaire, le serveur a ferme la connexion.\n");
+			return 0;
+		} else {
+			/*
+			 * on a recu "retour" octets en plus
+			 */
+			return dejaRecu + retour;
+		}
+	} else {
+		return dejaRecu;
+	}
+}
+
+/* Envoie des donnes au serveur en prcisant leur taille.
+ */
+int EmissionBinaire(char *donnees, size_t taille) {
+	int retour = 0;
+	retour = send(socketClient, donnees, taille, 0);
+	if(retour == -1) {
+		perror("Emission, probleme lors du send.");
+		return -1;
+	} else {
+		return retour;
+	}
+}
+
+
+/* Ferme la connexion.
+ */
+void Terminaison() {
+	close(socketClient);
+}
+
+
+
+/* ========================================
+ * Fonctions affichages
+ * ======================================== */
+
+
+/*
+ * affiche le menu
+ */
+void afficheMenu() {
+	printf("Choisissez une action: \n");
+	printf("1 - Emission d'un fichier\n");
+	printf("0 - quitter\n");
+}
+
+void gererEmission(char nomFichier[50]) {
+  
+ 	printf("Emission du fichier %s ToDo\n", nomFichier);
+	nomFichier[strlen(nomFichier)]='\n';
+	Emission(nomFichier);
+	
+}
+
+
+
+
+/* ========================================
+ * Fonctions traitements
+ * ======================================== */
+
+
+/*
+ * Renvoi un entier en fonction de la reponse reçue
+ */
+int decodageReq(char* reponse){
+  int resDecodage = 0;
+
+  // Décodage de la réponse connexion
+
+
+
+  return resDecodage ;
+}
+
+
+
+
+/*
+ * S'occupe de la connexion de l'utilisateur -1 si erreur
+ */
+
+int connexion(){
+  int resCo = 0;
+  char* login=NULL; // Entrer par l'utilisateur
+  char* password=NULL; // Entrer par l'utilisateur
+  char* requete_temp=NULL;
+  char* requete=NULL;
+
+  // Allocation de la mémoire pour les chaines
+  login = malloc((size_t)50); // (size_t) 50 caractères me parrait suffisant pour un login et mdp
+  password = malloc((size_t)50);
+  requete_temp = malloc((size_t)100);
+  requete = malloc((size_t)100);
+
+
+  // Phase utilisateur
+  printf("Entrer votre login : ");
+  fgets(login, 50,stdin);
+
+  password = getpass("Mot de passe : "); // Permet de cacher les caractères tapés
+ 
+  // Préparation de la chaine login
+  login[strlen(login)-1] = '\0';
+
+  // Création de la requête à emettre au serveur
+  requete = strcat(requete,"CON_USER \0"); 
+  requete_temp = strcat (login, " ");
+  requete_temp = strcat(requete_temp, password);
+  requete = strcat(requete, requete_temp);
+  
+  // Préparation de la chaine requete à l'emission => Verification : Requete OK
+  requete[strlen(requete)]='\n';
+
+  // Emission de la requete de connexion au serveur
+  if(Emission(requete)==0){
+    perror("\nErreur lors de l'emission de la requete de connexion");
+  }
+
+  /* printf("Avant les free"); */
+  
+  /* if(login != NULL ){ */
+  /*   free(login); */
+  /*   login = NULL; */
+  /* } */
+
+  /* if(password != NULL){ */
+  /*   free(password); */
+  /*   password = NULL; */
+  /* } */
+
+  /* if(requete_temp != NULL){ */
+  /*   free(requete_temp); */
+  /*   requete_temp = NULL; */
+  /* } */
+
+  /* if(requete != NULL){ */
+  /*   free(requete); */
+  /*   requete = NULL; */
+  /* } */
+
+  return resCo = 0;
+}
+
+
---
+++ Sources/serveur.c
@@ -0,0 +1,457 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include <sys/types.h>
+
+#ifdef WIN32
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#else
+#include <unistd.h>
+#include <netdb.h>
+#include <sys/socket.h>
+#include <sys/select.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#endif
+
+#include <errno.h>
+
+#include "serveur.h"
+
+#define TRUE 1
+#define FALSE 0
+#define LONGUEUR_TAMPON 4096
+
+
+#ifdef WIN32
+#define perror(x) printf("%s : code d'erreur : %d\n", (x), WSAGetLastError())
+#define close closesocket
+#define socklen_t int
+#endif
+
+/* Variables cachees */
+
+/* le socket d'ecoute */
+int socketEcoute;
+/* longueur de l'adresse */
+socklen_t longeurAdr;
+
+typedef struct {
+	/* le socket de service */
+	int socketService;
+	/* le tampon de reception */
+	char tamponClient[LONGUEUR_TAMPON];
+	int debutTampon;
+	int finTampon;
+} ClientDesc;
+
+/* Initialisation.
+ * Creation du serveur.
+ */
+int Initialisation() {
+	return InitialisationAvecService("13214");
+}
+
+/* Initialisation.
+ * Creation du serveur en prcisant le service ou numro de port.
+ * renvoie 1 si a c'est bien pass 0 sinon
+ */
+int InitialisationAvecService(char *service) {
+	int n;
+	const int on = 1;
+	struct addrinfo	hints, *res, *ressave;
+
+	#ifdef WIN32
+	WSADATA	wsaData;
+	if (WSAStartup(0x202,&wsaData) == SOCKET_ERROR)
+	{
+		printf("WSAStartup() n'a pas fonctionne, erreur : %d\n", WSAGetLastError()) ;
+		WSACleanup();
+		exit(1);
+	}
+	memset(&hints, 0, sizeof(struct addrinfo));
+    #else
+	bzero(&hints, sizeof(struct addrinfo));
+	#endif
+
+	hints.ai_flags = AI_PASSIVE;
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+
+	if ( (n = getaddrinfo(NULL, service, &hints, &res)) != 0)  {
+     		printf("Initialisation, erreur de getaddrinfo : %s", gai_strerror(n));
+     		return 0;
+	}
+	ressave = res;
+
+	do {
+		socketEcoute = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+		if (socketEcoute < 0)
+			continue;		/* error, try next one */
+
+		setsockopt(socketEcoute, SOL_SOCKET, SO_REUSEADDR, (char*)&on, sizeof(on));
+#ifdef BSD
+		setsockopt(socketEcoute, SOL_SOCKET, SO_REUSEPORT, &on, sizeof(on));
+#endif
+		if (bind(socketEcoute, res->ai_addr, res->ai_addrlen) == 0)
+			break;			/* success */
+
+		close(socketEcoute);	/* bind error, close and try next one */
+	} while ( (res = res->ai_next) != NULL);
+
+	if (res == NULL) {
+     		perror("Initialisation, erreur de bind.");
+     		return 0;
+	}
+
+	/* conserve la longueur de l'addresse */
+	longeurAdr = res->ai_addrlen;
+
+	freeaddrinfo(ressave);
+	/* attends au max 4 clients */
+	listen(socketEcoute, 4);
+	printf("Creation du serveur reussie.\n");
+
+	return 1;
+}
+
+/* Attends qu'un client se connecte.
+ */
+int AttenteClient(Client *cl) {
+	struct sockaddr *clientAddr;
+	char machine[NI_MAXHOST];
+	ClientDesc *client;
+
+	client = (ClientDesc*) malloc(sizeof(ClientDesc));
+	clientAddr = (struct sockaddr*) malloc(longeurAdr);
+	client->socketService = accept(socketEcoute, clientAddr, &longeurAdr);
+	if (client->socketService == -1) {
+		perror("AttenteClient, erreur de accept.");
+		free(clientAddr);
+		free(client);
+		return 0;
+	}
+	if(getnameinfo(clientAddr, longeurAdr, machine, NI_MAXHOST, NULL, 0, 0) == 0) {
+		printf("Client sur la machine d'adresse %s connecte.\n", machine);
+	} else {
+		printf("Client anonyme connecte.\n");
+	}
+	free(clientAddr);
+	/*
+	 * Reinit buffer
+	 */
+	client->debutTampon = 0;
+	client->finTampon = 0;
+	*cl = (void*) client;
+	return 1;
+}
+
+/* Recoit un message envoye par le serveur.
+ */
+char *Reception(Client cl) {
+	char message[LONGUEUR_TAMPON];
+	int index = 0;
+	int fini = FALSE;
+	int retour = 0;
+	ClientDesc *client = (ClientDesc*)cl;
+	while(!fini) {
+		/* on cherche dans le tampon courant */
+		while((client->finTampon > client->debutTampon) &&
+			(client->tamponClient[client->debutTampon]!='\n')) {
+			message[index++] = client->tamponClient[client->debutTampon++];
+		}
+		/* on a trouve si on a récupéré quelque chose et que le prochain caractère est \n */
+		if ((index > 0) && (client->tamponClient[client->debutTampon]=='\n')) {
+			message[index++] = '\n';
+			message[index] = '\0';
+			client->debutTampon++;
+			fini = TRUE;
+			/* printf("Etat tampon : debut = %d ; fin = %d ; prochain caractere : %d \n", client->debutTampon, client->finTampon, 
+				client->tamponClient[client->debutTampon]); */
+#ifdef WIN32
+			return _strdup(message);
+#else
+			return strdup(message);
+#endif
+		} else {
+			/* il faut en lire plus */
+			/* printf("Lecture socket\n"); */
+			client->debutTampon = 0;
+			retour = recv(client->socketService, client->tamponClient, LONGUEUR_TAMPON, 0);
+			/* printf("Reception de %d caracteres\n", retour); */
+			if (retour < 0) {
+				perror("Reception, erreur de recv.");
+				return NULL;
+			} else if(retour == 0) {
+				fprintf(stderr, "Reception, le client a ferme la connexion.\n");
+				return NULL;
+			} else {
+				/*
+				 * on a recu "retour" octets
+				 */
+				client->finTampon = retour;
+			}
+		}
+	}
+	return NULL;
+}
+
+/* Envoie un message au client.
+ * Attention, le message doit etre termine par \n
+ */
+int Emission(char *message, Client cl) {
+	int taille;
+	ClientDesc *client = (ClientDesc*)cl;
+	if(strstr(message, "\n") == NULL) {
+		fprintf(stderr, "Emission, Le message n'est pas termine par \\n.\n");
+		return 0;
+	}
+	taille = strlen(message);
+	if (send(client->socketService, message, taille,0) == -1) {
+        perror("Emission, probleme lors du send.");
+        return 0;
+	}
+	printf("Emission de %d caracteres.\n", taille+1);
+	return 1;
+}
+
+
+/* Recoit des donnees envoyees par le client.
+ */
+int ReceptionBinaire(char *donnees, size_t tailleMax, Client cl) {
+	size_t dejaRecu = 0;
+	int retour = 0;
+	ClientDesc *client = (ClientDesc*)cl;
+	/* on commence par recopier tout ce qui reste dans le tampon
+	 */
+	while((client->finTampon > client->debutTampon) && (dejaRecu < tailleMax)) {
+		donnees[dejaRecu] = client->tamponClient[client->debutTampon];
+		dejaRecu++;
+		client->debutTampon++;
+	}
+	/* si on n'est pas arrive au max
+	 * on essaie de recevoir plus de donnees
+	 */
+	if(dejaRecu < tailleMax) {
+		retour = recv(client->socketService, donnees + dejaRecu, tailleMax - dejaRecu, 0);
+		if(retour < 0) {
+			perror("ReceptionBinaire, erreur de recv.");
+			return -1;
+		} else if(retour == 0) {
+			fprintf(stderr, "ReceptionBinaire, le client a ferme la connexion.\n");
+			return 0;
+		} else {
+			/*
+			 * on a recu "retour" octets en plus
+			 */
+			return dejaRecu + retour;
+		}
+	} else {
+		return dejaRecu;
+	}
+}
+
+/* Envoie des donnes au client en prcisant leur taille.
+ */
+int EmissionBinaire(char *donnees, size_t taille, Client cl) {
+	int retour = 0;
+	ClientDesc *client = (ClientDesc*)cl;
+	retour = send(client->socketService, donnees, taille, 0);
+	if(retour == -1) {
+		perror("Emission, probleme lors du send.");
+		return -1;
+	} else {
+		return retour;
+	}
+}
+
+
+
+/* Ferme la connexion avec le client.
+ */
+void TerminaisonClient(Client cl) {
+	ClientDesc *client = (ClientDesc*)cl;
+	close(client->socketService);
+	free(client);
+}
+
+/* Arrete le serveur.
+ */
+void Terminaison() {
+	close(socketEcoute);
+}
+
+
+ 
+
+
+/* 
+ * Fonction afin de décoder les requêtes et les envoyer vers la fonction correspondante
+ */
+int decodeRequete(char* message, char* pathDB){
+  char* login;
+  char* extr;// utiliser pour l'extraction
+  char* password;
+  char* requete;
+
+
+  // Utilisé pour extraire une chaine grâce à un délimiteur ici espace
+  // Le premier prend tout ce qui est à droite de " "
+  requete = strtok(message," ");  
+  login = strtok(NULL, " "); // Le second tout ce qui est à droite du second delimiteur
+  password = strtok(NULL, " "); // etc..
+
+
+  if( strcmp(requete, "CON_USER") == 0){
+      
+    printf("\n\n\n %s ", login);
+    printf("\n\n\n %s ", password);
+  
+    verificationLog(login, password, pathDB);
+    return 1;
+  }
+
+  free(password);
+  free(requete);
+  free(login);
+
+  printf("Après free");
+
+  // Ici toutes les requêtes, faudrait ce mettre d'accord sur lesquelles on fait
+
+  
+  
+  return 0;
+
+
+
+}
+
+
+
+/*
+ * Fonction de verification d'information de connexion d'un utilisateur sur un client
+ * A intégrer dans une plus grande fonction "decodeReq"
+ */
+
+/*
+ * Authentification réussie , renvoie 0 sinon 1
+ */
+int verificationLog(char* login, char* password, char* databasePath){
+   char* ligneLog=NULL; // Lu dans le fichier => Utile pour faire les tests
+   char* reponse=NULL;
+   FILE* dataLog;
+   char* loginLu; 
+   char* mdpLu;
+
+
+  // Allocation de la mémoire
+  ligneLog = malloc((size_t)150);
+ 
+
+  //Ouverture du fichier "database"
+  dataLog = fopen(databasePath,"r");
+  
+  if(dataLog!= NULL){
+    printf("\n\nOuverture du fichier\n"); //=> OK
+
+    //On parcourt le fichier avec des strncmp
+    do{
+      fgets(ligneLog, 150 , dataLog);
+      
+      // On récupere le login lu dans le fichier
+      loginLu = strtok(ligneLog, " ");
+      
+      // Test du login
+      if (strcmp(login, loginLu)==0){
+	mdpLu = strtok(NULL, " "); // On recupere le mdp Lu dans le fichier
+	if(strcmp(password, mdpLu)==0){ // On teste, si Oui on retourne 0 => Authentification réussie
+	  printf("\n\t Authentification réussie : %s\n ", login);
+	  fclose(dataLog);
+	  
+
+	  // Desalocation de la mémoire
+	  if(ligneLog != NULL){
+	    free(ligneLog);
+	    ligneLog = NULL;
+	  }
+	  if(loginLu !=NULL){
+	    free(loginLu);
+	    loginLu = NULL;
+	  }
+	  if(mdpLu != NULL){
+	    free(mdpLu);
+	    mdpLu = NULL;
+	  }
+
+
+
+
+	  return 0;
+	}
+      }
+      
+    }while(feof(dataLog)== 0);
+
+
+  }else{
+    perror("\nErreur lors de l'ouverture du fichier");
+    // Fermeture du fichier et liberation de la mémoire lors d'une erreur d'ouverture
+    /* free(loginLu); */
+   
+
+
+  }
+  
+  return 1; // Authentification échouée
+
+
+  fclose(dataLog);
+  free(ligneLog);
+  
+
+}
+
+//** Récupérer et modifier les fonctions découperRequêtes et executerRequetes
+
+void *thread(void *arg) {
+	/* On récupère le paramètre. Ici de type void* */
+	Client cl = arg;
+	char* message = NULL;
+	char* path = "/home/tibo/Cours/ProjetC/ressources/essai.txt";
+
+
+		do {
+		if(message != NULL) {
+			free(message);
+		}
+
+
+	       	message = Reception(cl);
+
+		// On décode la requête envoyé par le client
+		decodeRequete(message, path);
+
+
+		if(message != NULL) {
+		  // printf("J'ai recu: %s\n Je vais émettre maitenant", message);
+		  // Decoupage et execution de la requete */
+		  //  Emission("BienRecuCl\n",cl) ;
+		  // EmissionBinaire(char *donnees, size_t taille, cl)
+		       		}
+	} while(message != NULL);
+
+	TerminaisonClient(cl);
+	pthread_exit(NULL);
+}
+
+
+
+
+//Retourne 
+
+
+
+
